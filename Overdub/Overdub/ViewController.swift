003b06519ff1aef4fedc309572cb4dbec2b931bb
Naoki Tsutsui <naokitsutsui@me.com>
2014-06-10 00:13:30 +0900

    初期プロジェクトを追加

---
+++ Overdub/Overdub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Overdub
+//
+//  Created by Naoki Tsutsui on 2014/06/08.
+//  Copyright (c) 2014年 Naoki Tsutsui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
