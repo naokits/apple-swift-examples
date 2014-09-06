e8cbfb448856ee302ab106a07eafa5a251234059
Naoki Tsutsui <naokitsutsui@me.com>
2014-09-06 23:55:20 +0900

    初期プロジェクトを追加

---
+++ SimpleFlickr/SimpleFlickr/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SimpleFlickr
+//
+//  Created by Naoki Tsutsui on 2014/09/06.
+//  Copyright (c) 2014年 Naoki Tsutsui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
